69a4bc68e4f7568a362794ac455fd97fed731908
abbshr <abbshr@outlook.com>
2014-03-27 18:49:17 +0800

    add dependence

--- main.c
+++ main.c
@@ -7,20 +7,20 @@ typedef struct College
 {
     char* cName;
     char* state;
-    char* enrollment;
+    int enrollment;
 } College;
 
 typedef struct Student
 {
-    char* sID;
+    int sID;
     char* sName;
-    char* GPA;
-    char* sizeHS;
+    float GPA;
+    int sizeHS;
 } Student;
 
 typedef struct Apply
 {
-    char* sID;
+    int sID;
     char* cName;
     char* major;
     char* decision;
@@ -32,7 +32,7 @@ static int view_all_callback(void* data, int argc, char** argv, char** azColName
     fprintf(stderr, "%s:\n", (const char*)data);
     for (i = 0; i < argc; i++)
     {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        printf("%s = %s, ", azColName[i], argv[i] ? argv[i] : "NULL");
     }
     printf("\n");
     return 0;
@@ -40,11 +40,6 @@ static int view_all_callback(void* data, int argc, char** argv, char** azColName
 
 static int insert_table_callback(void* NotUsed, int argc, char** argv, char** azColName)
 {
-    int i = 0;
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
     return 0;
 }
 
@@ -60,7 +55,7 @@ int main(int argc, char* argv[])
     int rc;
     char* sql;
     const char* data = "Callback function called";
-    /* åˆ›å»ºæ•°æ®åº“è¿žæŽ¥ */
+    /* ´´½¨Êý¾Ý¿âÁ¬½Ó */
     rc = sqlite3_open("DBforCourse.db3", &db);
     if (rc)
     {
@@ -70,7 +65,7 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Opened database successfully\n");
     }
 
-    /* å»ºè¡¨*/
+    /* ½¨±í*/
     sql = "create table College(cName text, state text, enrollment int);";
     rc = sqlite3_exec(db, sql, create_table_callback, 0, &zErrMsg);
     if (rc != SQLITE_OK)
@@ -99,14 +94,14 @@ int main(int argc, char* argv[])
         fprintf(stdout, "Apply Table created successfully\n");
     }
 
-    /* åŠ è½½æ–‡ä»¶*/
+    /* ¼ÓÔØÎÄ¼þ*/
     College college[4];
     Student student[12];
     Apply apply[19];
     FILE* fp;
-    char line[30];
+    char line[100];
     char delims[] = ",";
-    char* result = NULL;
+    char* result;
     int i = 0;
     if(fp = fopen("dbcollege.txt", "r"))
     {
@@ -117,97 +112,106 @@ int main(int argc, char* argv[])
             result = strtok(NULL, delims);
             college[i].state = result;
             result = strtok(NULL, delims);
-            college[i].enrollment = result;
-            result = NULL;
-            //printf("%s, %s, %s\n", college[i].cName, college[i].state, college[i].enrollment);
+            college[i].enrollment = atoi(result);
+            //printf("%s, %s, %d\n", college[i].cName, college[i].state, college[i].enrollment);
+            sql = sqlite3_mprintf("insert into College(cName, state, enrollment) values('%s', '%s', '%d');", college[i].cName, college[i].state, college[i].enrollment);
+            rc = sqlite3_exec(db, sql, insert_table_callback, 0, &zErrMsg);
+            if (rc != SQLITE_OK)
+            {
+                fprintf(stderr, "SQL error: %s\n", zErrMsg);
+                sqlite3_free(zErrMsg);
+            } else {
+                fprintf(stdout, "operations done successfully\n");
+            }
+            sqlite3_free(sql);
             i++;
         }
         fclose(fp);
     }
+    result = NULL;
     i = 0;
     if(fp = fopen("dbstudent.txt", "r"))
     {
         while (fgets(line, 100, fp) != NULL)
         {
             result = strtok(line, delims);
-            student[i].sID = result;
+            student[i].sID = atoi(result);
             result = strtok(NULL, delims);
             student[i].sName = result;
             result = strtok(NULL, delims);
-            student[i].GPA = result;
+            student[i].GPA = atof(result);
             result = strtok(NULL, delims);
-            student[i].sizeHS = result;
-            result = NULL;
-            //printf("%s, %s, %s, %s\n", student[i].sID, student[i].sName, student[i].GPA, student[i].sizeHS);
+            student[i].sizeHS = atoi(result);
+            //printf("%d, %s, %f, %d\n", student[i].sID, student[i].sName, student[i].GPA, student[i].sizeHS);
+            sql = sqlite3_mprintf("insert into Student(sID, sName, GPA, sizeHS) values('%d', '%s', '%f', '%d');", student[i].sID, student[i].sName, student[i].GPA, student[i].sizeHS);
+            rc = sqlite3_exec(db, sql, insert_table_callback, 0, &zErrMsg);
+            if (rc != SQLITE_OK)
+            {
+                fprintf(stderr, "SQL error: %s\n", zErrMsg);
+                sqlite3_free(zErrMsg);
+            } else {
+                fprintf(stdout, "operations done successfully\n");
+            }
+            sqlite3_free(sql);
             i++;
         }
         fclose(fp);
+
     }
+    result = NULL;
     i = 0;
     if(fp = fopen("dbapply.txt", "r"))
     {
         while (fgets(line, 100, fp) != NULL)
         {
             result = strtok(line, delims);
-            apply[i].sID = result;
+            apply[i].sID = atoi(result);
             result = strtok(NULL, delims);
             apply[i].cName = result;
             result = strtok(NULL, delims);
             apply[i].major = result;
             result = strtok(NULL, delims);
             apply[i].decision = result;
-            result = NULL;
-            //printf("%s, %s, %s, %s\n", apply[i].sID, apply[i].cName, apply[i].major, apply[i].decision);
+            //printf("%d, %s, %s, %s\n", apply[i].sID, apply[i].cName, apply[i].major, apply[i].decision);
+            sql = sqlite3_mprintf("insert into Apply(sID, cName, major, decision) values('%d', '%s', '%s', '%s');", apply[i].sID, apply[i].cName, apply[i].major, apply[i].decision);
+            rc = sqlite3_exec(db, sql, insert_table_callback, 0, &zErrMsg);
+            if (rc != SQLITE_OK)
+            {
+                fprintf(stderr, "SQL error: %s\n", zErrMsg);
+                sqlite3_free(zErrMsg);
+            } else {
+                fprintf(stdout, "operations done successfully\n");
+            }
+            sqlite3_free(sql);
             i++;
         }
         fclose(fp);
     }
-    i = 0;
 
-    /* å¯¼å…¥è¡¨*/
-    while (i < 4) {
-        sql = sqlite3_mprintf("insert into College(cName, state, enrollment) values('%s', '%s', '%d');", college[i].cName, college[i].state, atoi(college[i].enrollment));
-        rc = sqlite3_exec(db, sql, insert_table_callback, 0, &zErrMsg);
-        if (rc != SQLITE_OK)
-        {
-            fprintf(stderr, "SQL error: %s\n", zErrMsg);
-            sqlite3_free(zErrMsg);
-        } else {
-            fprintf(stdout, "operations done successfully\n");
-        }
-        i++;
-        sqlite3_free(sql);
-    }
-    i = 0;
-    while (i < 12) {
-        sql = sqlite3_mprintf("insert into Student(sID, sName, GPA, sizeHS) values('%d', '%s', '%f', '%d');", atoi(student[i].sID), student[i].sName, atof(student[i].GPA), atoi(student[i].sizeHS));
-        rc = sqlite3_exec(db, sql, insert_table_callback, 0, &zErrMsg);
-        if (rc != SQLITE_OK)
-        {
-            fprintf(stderr, "SQL error: %s\n", zErrMsg);
-            sqlite3_free(zErrMsg);
-        } else {
-            fprintf(stdout, "operations done successfully\n");
-        }
-        i++;
-        sqlite3_free(sql);
-    }
-    i = 0;
-    while (i < 19) {
-        sql = sqlite3_mprintf("insert into Apply(sID, cName, major, decision) values('%d', '%s', '%s', '%s');", atoi(apply[i].sID), apply[i].cName, apply[i].major, apply[i].decision);
-        rc = sqlite3_exec(db, sql, insert_table_callback, 0, &zErrMsg);
-        if (rc != SQLITE_OK)
-        {
-            fprintf(stderr, "SQL error: %s\n", zErrMsg);
-            sqlite3_free(zErrMsg);
-        } else {
-            fprintf(stdout, "operations done successfully\n");
-        }
-        i++;
-        sqlite3_free(sql);
+    sql = "select * from College";
+    rc = sqlite3_exec(db, sql, view_all_callback, (void*)data, &zErrMsg);
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
     }
 
-    /* select in Student GPA > 3.6, return sID, sName and GPA */
+    sql = "select * from Student";
+    rc = sqlite3_exec(db, sql, view_all_callback, (void*)data, &zErrMsg);
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+
+    sql = "select * from Apply";
+    rc = sqlite3_exec(db, sql, view_all_callback, (void*)data, &zErrMsg);
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+    /* select in Student GPA > 3.6, return sID, sName and GPA*/
     sql = "select sID, sName, GPA from Student where GPA > 3.6;";
     rc = sqlite3_exec(db, sql, view_all_callback, (void*)data, &zErrMsg);
     if (rc != SQLITE_OK)
@@ -219,7 +223,7 @@ int main(int argc, char* argv[])
     }
 
     /* select in Apply and Student distinct, return sName, cName*/
-    sql = "select distinct sName, cName from Student nature join Apply;";
+    sql = "select distinct sName, cName from Student natural join Apply;";
     rc = sqlite3_exec(db, sql, view_all_callback, (void*)data, &zErrMsg);
     if (rc != SQLITE_OK)
     {
@@ -230,7 +234,7 @@ int main(int argc, char* argv[])
     }
 
     /* select*/
-    sql = "select sName, GPA, decision from Student nature join Apply where sizeHS < 1000 and major = \"CS\" and cName = \"Stanford\";";
+    sql = "select sName, GPA, decision from Student, Apply where Student.sID = Apply.sID and sizeHS < 1000 and major = \"CS\" and cName = \"Stanford\";";
     rc = sqlite3_exec(db, sql, view_all_callback, (void*)data, &zErrMsg);
     if (rc != SQLITE_OK)
     {
@@ -241,7 +245,7 @@ int main(int argc, char* argv[])
     }
 
     /**/
-    sql = "select * from Apply nature join Student order by GPA desc, enrollment;";
+    sql = "select distinct * from Apply, Student, College where Student.sID = Apply.sID and Apply.cName = College.cName order by GPA desc, enrollment;";
     rc = sqlite3_exec(db, sql, view_all_callback, (void*)data, &zErrMsg);
     if (rc != SQLITE_OK)
     {
@@ -263,7 +267,7 @@ int main(int argc, char* argv[])
     }
 
     /**/
-    sql = "select max(cName) from College;";
+    sql = "select cName, max(enrollment) from College;";
     rc = sqlite3_exec(db, sql, view_all_callback, (void*)data, &zErrMsg);
     if (rc != SQLITE_OK)
     {
@@ -274,7 +278,7 @@ int main(int argc, char* argv[])
     }
 
     /**/
-    sql = "select * from College nature join Student nature join Apply;";
+    sql = "select * from College natural join Student natural join Apply;";
     rc = sqlite3_exec(db, sql, view_all_callback, (void*)data, &zErrMsg);
     if (rc != SQLITE_OK)
     {
@@ -285,7 +289,7 @@ int main(int argc, char* argv[])
     }
 
     /**/
-    sql = "select cName from Apply where count(major) < 5 group by cName;";
+    sql = "select cName from Apply group by cName having count(major) < 5;";
     rc = sqlite3_exec(db, sql, view_all_callback, (void*)data, &zErrMsg);
     if (rc != SQLITE_OK)
     {
